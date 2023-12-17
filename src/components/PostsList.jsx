0a4bbd8945a998a798809edf3699fc7b30f428c8
mazenaboelanin <mazenaboelanin@gmail.com>
2023-12-17 02:43:34 +0200

    create and use new post component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostsList.module.css'
 
 function PostsList() {
   return (
+    <>
+    <NewPost />
     <ul className={classes.posts}>
       <Post author="Mazen" body="React overview" />
       <Post author="Ahmed" body="React Full knowledge"/>
     </ul>
+    </>
   );
 }
 
