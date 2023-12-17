b6e82dac75e57fd0ba49b0a6a57334c391133d8a
mazenaboelanin <mazenaboelanin@gmail.com>
2023-12-17 02:15:58 +0200

    add styles to post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,10 @@
+import classes from './Post.module.css';
+
 function Post(props) {
   return (
-    <div>
-      <h3>{props.author}</h3>
-      <p>{props.body}</p>
+    <div className={classes.post}>
+      <h3 className={classes.author}>{props.author}</h3>
+      <p className={classes.body}>{props.body}</p>
     </div>
   );
 }
