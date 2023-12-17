fdd04ad28bb6918331d9c04950ca4aac4b9d1f4b
mazenaboelanin <mazenaboelanin@gmail.com>
2023-12-18 01:54:20 +0200

    add hide modal handler to close the popup form

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,10 +6,13 @@ import Modal from './Modal';
 import classes from './PostsList.module.css'
 
 function PostsList() {
-  const [ enteredBody, setEnteredBody] = useState('');
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
 
-  const [ enteredAuthor, setEnteredAuthor] = useState('');
-
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function changeBodyHandler(event) {
     setEnteredBody(event.target.value);
@@ -19,15 +22,21 @@ function PostsList() {
     setEnteredAuthor(event.target.value);
   }
 
+  let modalContent;
+
+  if(modalIsVisible){
+    modalContent = <Modal onClose={hideModalHandler}>
+    <NewPost
+      onBodyChange={changeBodyHandler}
+      onAuthorChange={changeAuthorHandler}
+    />
+  </Modal>
+  }
 
   return (
     <>
-    <Modal>
-      <NewPost
-        onBodyChange={changeBodyHandler}
-        onAuthorChange={changeAuthorHandler}
-      />
-    </Modal>
+    { modalContent }
+
     <ul className={classes.posts}>
       <Post author={enteredAuthor} body={enteredBody} />
       <Post author="Ahmed" body="React Full knowledge"/>
